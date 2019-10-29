4bd5696920d203f1dfc9ea4679c5e03fa2a476a8
pandalz <roayfx000@gmail.com>
2019-10-29 12:39:55 -0600

    Added first tests for testing and generation

---
+++ primertalzgo_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPrimertalzgo(t *testing.T) {
+
+	t.Run("Single number decomposition", func(t *testing.T) {
+
+	})
+
+	t.Run("Gen from square", func(t *testing.T) {
+
+	})
+
+}
