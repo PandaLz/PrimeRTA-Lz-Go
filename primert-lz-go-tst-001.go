a4f915dc7bdd4016e7ce8d54a3c9fb6373096615
pandalz <roayfx000@gmail.com>
2019-11-05 18:16:11 -0600

    Added variables and output for the reminder of square root and pair test

--- primert-lz-go-tst-001.go
+++ primert-lz-go-tst-001.go
@@ -7,13 +7,35 @@ import (
 
 func main() {
 
+	// one := big.NewInt(1)
 	//test single number
-	var numToTest, numSqrt big.Int
-	numToTest.SetInt64(65)
+	var zero, two big.Int
+	var numToTest, numSqrt, numSqrtReminder, sqrtRemMod big.Int
+	var isPair bool = false
+
+	// set the number to test
+	numToTest.SetInt64(85)
+	zero.SetInt64(0)
+	two.SetInt64(2)
 	// or one := big.NewInt(1)
+
+	// get Square root of number to test
 	numSqrt.Sqrt(&numToTest)
 
-	fmt.Printf("Number to test     : %v\n", numToTest)
-	fmt.Printf("Number Square root : %v\n", numSqrt)
+	// get reminder of number to test / square root
+	numSqrtReminder.Mod(&numToTest, &numSqrt)
+
+	// Know if numSqrt is odd
+	sqrtRemMod.Mod(&numSqrt, &two)
+
+	if 0 == sqrtRemMod.Cmp(&zero) {
+		isPair = true
+	}
+
+	fmt.Printf("Number to test       : %v\n", numToTest)
+	fmt.Printf("Number Square root   : %v\n", numSqrt)
+	fmt.Printf("Square root reminder : %v\n", numSqrtReminder)
+	fmt.Printf("Mod of square root reminder  : %v\n", sqrtRemMod)
+	fmt.Printf("Square root is pair  : %v\n", isPair)
 
 }
