3e074c7cbdcc66b3f2a5ba6d49b4d1dc6d6fcbfe
pandalz <PandaLz@users.noreply.github.com>
2019-11-06 18:38:23 -0600

    Added a formatting to output, added procedure to get mid reminder

--- primert-lz-go-tst-001.go
+++ primert-lz-go-tst-001.go
@@ -9,13 +9,16 @@ func main() {
 
 	// one := big.NewInt(1)
 	//test single number
-	var zero, two big.Int
+	var zero, one, two big.Int
 	var numToTest, numSqrt, numSqrtReminder, sqrtRemMod big.Int
+	var halfNumSqrt, midRem big.Int
 	var isPair bool = false
 
 	// set the number to test
 	numToTest.SetInt64(85)
+
 	zero.SetInt64(0)
+	one.SetInt64(1)
 	two.SetInt64(2)
 	// or one := big.NewInt(1)
 
@@ -23,19 +26,44 @@ func main() {
 	numSqrt.Sqrt(&numToTest)
 
 	// get reminder of number to test / square root
-	numSqrtReminder.Mod(&numToTest, &numSqrt)
+	// numSqrtReminder.Mod(&numToTest, &numSqrt)
+	// missing math/big Sqrt with reminder method
+	// calculating from number to test - square root squared...
+	numSqrtReminder.Mul(&numSqrt, &numSqrt)
+	numSqrtReminder.Sub(&numToTest, &numSqrtReminder)
 
-	// Know if numSqrt is odd
+	// Know if numSqrt is pair
 	sqrtRemMod.Mod(&numSqrt, &two)
 
 	if 0 == sqrtRemMod.Cmp(&zero) {
 		isPair = true
 	}
 
-	fmt.Printf("Number to test       : %v\n", numToTest)
-	fmt.Printf("Number Square root   : %v\n", numSqrt)
-	fmt.Printf("Square root reminder : %v\n", numSqrtReminder)
-	fmt.Printf("Mod of square root reminder  : %v\n", sqrtRemMod)
-	fmt.Printf("Square root is pair  : %v\n", isPair)
+	// process according to pair test of numSqrt
+	if isPair {
+		// get the half of the reminder
+		halfNumSqrt.Div(&numSqrt, &two)
+		// get reminder for middle column
+		midRem.Add(&numSqrtReminder, &one)
+		midRem.Div(&midRem, &two)
+	} else {
+		// the correct way would be -1 / 2
+		halfNumSqrt.Div(&numSqrt, &two)
+		// get reminder for middle column
+		midRem.Div(&numSqrtReminder, &two)
+	}
+
+	fmt.Printf("------------------------------------\n")
+	fmt.Printf("-------Initial data required--------\n")
+	fmt.Printf("------------------------------------\n")
+	fmt.Printf("Number to test               : %v\n", numToTest.String())
+	fmt.Printf("Number Square root           : %v\n", numSqrt.String())
+	fmt.Printf("Square root reminder         : %v\n", numSqrtReminder.String())
+	fmt.Printf("Mod2 of square root reminder : %v\n", sqrtRemMod.String())
+	fmt.Printf("Square root is pair          : %v\n", isPair)
+	fmt.Printf("------------------------------------\n")
+	fmt.Printf("Half square root             : %v\n", halfNumSqrt.String())
+	fmt.Printf("mid reminder                 : %v\n", midRem.String())
+	fmt.Printf("------------------------------------\n")
 
 }
